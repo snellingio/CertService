49fcfd28ccbc69507e14562957cfdc3023a8e03e
Sam Snelling <sam@onroi.com>
2017-03-29 19:47:59 -0500

    Add docblock for properties

--- app/Models/Certificate.php
+++ app/Models/Certificate.php
@@ -1,11 +1,21 @@
 <?php
-declare (strict_types = 1);
+declare (strict_types=1);
 
 namespace Model;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * @property string certificate
+ * @property string domain_id
+ * @property string issuer
+ * @property string issuerChain
+ * @property string serial_number
+ * @property string uuid
+ * @property string valid_from
+ * @property string valid_to
+ */
 class Certificate extends Model
 {
     use SoftDeletes;
